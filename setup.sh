e6a18ece171459c02205556dfa8eb678ebad4ff7
Kate Hart <codehearts@users.noreply.github.com>
2013-10-05 21:25:02 -0400

    Added basic shell script to symlink Vim files

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SYSTEM="none"
+
+while test $# -gt 0; do
+	case "$1" in
+		-h|--help)
+			echo "$package - Symlinks dotfiles into place"
+			echo " "
+			echo "$package [options]"
+			echo " "
+			echo "options:"
+			echo "-h, --help                show brief help"
+			echo "--mac                     setup symlinks for a Mac system"
+			echo "--arch                    setup symlinks for an Arch Linux system"
+			exit 0
+			;;
+		--mac)
+			shift
+			SYSTEM="mac"
+			shift
+			;;
+		--arch)
+			shift
+			SYSTEM="arch"
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+
+ln -s $PWD/gitignore ~/.gitignore
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/vim/bundle ~/vim/bundle
+ln -s $PWD/vim/color ~/vim/color
