ad2003f30f6db0d3326b7860535cce2de3448c2f
Kate Hart <codehearts@users.noreply.github.com>
2014-12-30 13:09:21 -0500

    Cleaned up xmonad.hs

--- arch/xmonad/xmonad.hs
+++ arch/xmonad/xmonad.hs
@@ -7,51 +7,34 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.Spacing
--- Used for xmonad monitors
---import XMonad.Layout.LayoutModifier
---import XMonad.Layout.Monitor
 
-main = do xmonad $ ewmh xfceConfig
+main = do xmonad $ ewmh xfceConfig 
 		{ modMask = mod4Mask
 		, terminal = "xfce4-terminal"
-		, normalBorderColor  = "#000000"
+		, normalBorderColor = "#000000"
 		, focusedBorderColor = "#f956a2"
 		, borderWidth = 0
-		-- Append `manageMonitor panel` if using the panel monitor
 		, manageHook =  manageDocks <+> myManageHook
-		, layoutHook = myLayout
+		, layoutHook = avoidStruts $ myLayout
 		, logHook = myLogHook
 		, handleEventHook = handleEventHook xfceConfig <+> fullscreenEventHook
 		}`additionalKeys`
 		[(( mod4Mask, xK_v), spawn "gvim")
 		,(( mod4Mask, xK_w), spawn "firefox")
-		,(( mod4Mask .|. shiftMask, xK_w), spawn "firefox -private")
+		,(( mod4Mask .|. shiftMask, xK_w), spawn "firefox -private-window")
 		,(( mod4Mask, xK_a), spawn "xfce4-terminal -e music")
-		,(( mod4Mask, xK_e), spawn "xfce4-terminal -e checkmail")
+		,(( mod4Mask, xK_e), spawn "xfce4-terminal -e email")
 		,(( mod4Mask, xK_c), spawn "xfce4-terminal -e irc")
 		,(( mod4Mask, xK_z), spawn "zathura --fork")
 		,(( mod4Mask, xK_d), spawn "xfce4-terminal --drop-down")
 		,(( mod4Mask, xK_p), spawn "xfce4-popup-whiskermenu")
-		-- Replace `xfce4-panel` with `tint2` if using tint2
 		,(( mod4Mask, xK_x), spawn "sh -c 'if pgrep xfce4-panel; then pkill xfce4-panel; else xfce4-panel --disable-wm-check; fi'")
-		--,(( mod4Mask, xK_s), spawn "sh -c 'if pgrep conky; then pkill conky; else ~/.conky/conkystart.sh; fi'")
-		-- Refreshes monitors, useful if using the panel monitor
-		--,(( mod4Mask, xK_u), broadcastMessage ToggleMonitor >> refresh)
 		]
 
-{-
- -- xmonad monitor for a tint2 panel
-panel = monitor {
-      prop = ClassName "Tint2"
-    , rect = Rectangle 0 (1080-30) 1920 30
-    , persistent = True
-    , visible = True
-    , name = "panel"
-    }
--}
-
--- Prepend `ModifiedLayout panel` if using the panel monitor
-myLayout = spacing 10 $ avoidStruts $ layoutHintsWithPlacement (0.5, 0.5) (Tall 1 (3/100) (1/2)) ||| Full
+myLayout = spacey ||| full
+	where
+		spacey = spacing 10 $ layoutHintsWithPlacement (0.5, 0.5) (Tall 1 (3/100) (1/2))
+		full   = Full
 
 myLogHook :: X()
 myLogHook = fadeInactiveLogHook fadeAmount
@@ -59,10 +42,12 @@ myLogHook = fadeInactiveLogHook fadeAmount
 
 myManageHook = composeAll
 	[ className =? "Xfce4-notifyd" --> doIgnore
-	, className =? "Gvbam" --> doFloat
-	, className =? "Skype" --> doFloat
-	, className =? "Conky" --> doIgnore
-	, className =? "Tint2" --> doHideIgnore
-	, className =? "Xfdesktop" --> doIgnore
+	, className =? "Conky"         --> doIgnore
+	, className =? "Xfdesktop"     --> doIgnore
+
+	, className =? "Gvbam"   --> doFloat
+	, className =? "Skype"   --> doFloat
 	, className =? "Wrapper" --> doFloat
+
+	, className =? "Tint2" --> doHideIgnore
 	]
