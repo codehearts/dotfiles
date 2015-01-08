c8a782a383a5d111b1a2b2dd78f63b1979f8df07
Kate Hart <codehearts@users.noreply.github.com>
2015-01-07 21:09:45 -0600

    Added trackpad gesture configuration for OS X, minor bug fixes

--- osx/osx-setup.sh
+++ osx/osx-setup.sh
@@ -76,10 +76,22 @@ defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 # Save to disk (not to iCloud) by default
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
+# Play a feedback sound when adjusting volume
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 1
+
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: use four finger horizontal swipe to move between fullscreen apps
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerHorizSwipeGesture -int 2
+
+# Trackpad: use four finger vertical swipe for mission control
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 0
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadFourFingerVertSwipeGesture -int 2
+
 
 # Use scroll gesture with the Ctrl (^) modifier key to zoom
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
@@ -123,19 +135,19 @@ defaults write com.apple.terminal StringEncodings -array 4
 # Use my own custom theme
 TERM_PROFILE='inuBASHiri Dark Plain';
 CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
-TMP_DIR='${HOME}/osx-setup-tmp';
+TMP_DIR="${HOME}/osx-setup-tmp";
 if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
 	mkdir -p $TMP_DIR
 
 	curl https://raw.githubusercontent.com/nejsan/dotfiles/master/osx/terminal-themes/inuBASHiri%20Dark%20Plain.terminal -o "${TMP_DIR}/${TERM_PROFILE}.terminal"
-	open "~/osx-setup-tmp/${TERM_PROFILE}.terminal";
+	open "${TMP_DIR}/${TERM_PROFILE}.terminal";
 	sleep 1; # Wait a bit to make sure the theme is loaded
 
 	curl https://raw.githubusercontent.com/nejsan/dotfiles/master/osx/terminal-themes/inuBASHiri%20Plain.terminal -o "${TMP_DIR}/inuBASHiri Plain.terminal"
-	open "~/osx-setup-tmp/inuBASHiri Plain.terminal";
+	open "${TMP_DIR}/inuBASHiri Plain.terminal";
 	sleep 1; # Wait a bit to make sure the theme is loaded
 
-	rm -rf ~/osx-setup-tmp
+	rm -rf $TMP_DIR
 
 	defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
 	defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
