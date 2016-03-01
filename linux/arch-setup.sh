dc253249f426b46924adfbf7d84b9df287788725
Kate Hart <codehearts@users.noreply.github.com>
2016-03-01 11:34:03 -0500

    Arch: Refactor arch-setup.sh

--- linux/arch-setup.sh
+++ linux/arch-setup.sh
@@ -51,7 +51,7 @@ mark_for_installation () {
 
 echo "Performing system upgrade"
 
-$pkg_mgr -Syu
+$pkg_mgr -Syyu
 
 ########################################
 # Dialog Software Install
@@ -74,6 +74,13 @@ if ! $installed; then
 	sudo systemctl enable ntpd
 fi
 
+is_command_installed git
+if ! $installed; then
+	echo "Installing git"
+
+	$pkg_mgr -S git
+fi
+
 # Include the dialog script
 . "$linux_dir/../scripts/dialog.sh"
 . "$linux_dir/../scripts/file_tools.sh"
@@ -88,13 +95,11 @@ if ! $installed; then
 	yes_no "Install yaourt?"
 	if $yes; then
 		$pkg_mgr --noconfirm -S base-devel
-		curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
-		tar zxvf package-query.tar.gz
+		git clone https://aur.archlinux.org/package-query.git
 		cd package-query
 		makepkg -si
 		cd ..
-		curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
-		tar zxvf yaourt.tar.gz
+		git clone https://aur.archlinux.org/yaourt.git
 		cd yaourt
 		makepkg -si
 		cd ..
@@ -129,13 +134,6 @@ checklist "Choose software to install:" packages[@] defaults[@]
 for choice in $choices; do
 	choice=${packages[$choice]} # Get the name of the choice
 	mark_for_installation $choice
-
-	case $choice in
-	virtualbox)
-		# Install a related necessary package
-		mark_for_installation virtualbox-guest-utils
-	;;
-	esac
 done
 
 ########################################
@@ -151,61 +149,31 @@ if $yes; then
 
 	clear
 	mark_for_installation xorg-server
-	mark_for_installation xf86-input-synaptics
-
-	packages=( xfce4-mixer xfce4-notifyd xfce4-panel xfce4-power-manager  )
-	defaults=( "on"        "on"          "on"        "on"                 )
-	packages+=(xfce4-session xfce4-settings xfce4-terminal xfdesktop xfwm4)
-	defaults+=("on"          "on"           "on"           "off"     "off")
-
-	checklist "Choose XFCE components to install:" packages[@] defaults[@]
-
-	# Process user choices
-	panel_installed=false
-	for choice in $choices; do
-		choice=${packages[$choice]} # Get the name of the choice
-		mark_for_installation $choice
-
-		case $choice in
-		xfce4-panel)
-			panel_installed=true
-			;;
-		esac
-	done
-
-	# Allow the user to install XFCE Panel goodies
-	if $panel_installed; then
-		packages=( xfce4-datetime-plugin xfce4-mailwatch-plugin xfce4-weather-plugin)
-		defaults=( "on"                  "on"                   "on"                )
-		packages+=(xfce4-whiskermenu-plugin                                         )
-		defaults+=("on"                                                             )
-
-		checklist "Choose XFCE panel goodies to install:" packages[@] defaults[@]
-
-		# Process user choices
-		for choice in $choices; do
-			choice=${packages[$choice]} # Get the name of the choice
-			mark_for_installation $choice
-		done
-	fi
+	mark_for_installation xorg-xrdb
+	mark_for_installation xorg-xbacklight
+	mark_for_installation xf86-input-libinput
 
 	########################################
 	# Graphical Software
 	########################################
 
-	packages=( accountsservice adobe-source-sans-pro-fonts chromium feh  firefox)
-	defaults=( "on"            "on"                        "off"    "on" "on"   )
-	packages+=(gvim lightdm-gtk-greeter rdesktop scrot slock tigervnc           )
-	defaults+=("on" "off"               "on"     "on"  "on"  "on"               )
-	packages+=(vagrant virtualbox xmonad-contrib zathura-pdf-mupdf              )
-	defaults+=("on"    "on"       "on"           "on"                           )
+	packages=( accountsservice adobe-source-sans-pro-fonts bspwm chromium       )
+	defaults=( "on"            "on"                        "on"  "off"          )
+	packages+=(compton dunst feh  firefox gvim lemonbar-xft-git                 )
+	defaults+=("on"    "on"  "on" "on"    "on" "on"                             )
+	packages+=(lightdm-gtk-greeter rdesktop rxvt-unicode scrot sxhkd slock      )
+	defaults+=("off"               "off"    "on"         "on"  "on"  "off"      )
+	packages+=(tigervnc vagrant virtualbox zathura-pdf-mupdf                    )
+	defaults+=("on"     "on"    "on"       "on"                                 )
 
 	# Add yaourt packages if yaourt was installed
 	if [ "$pkg_mgr" == "yaourt" ]; then
-		packages+=(compton dropbox numix-circle-icon-theme-git  gcalert google-chrome)
-		defaults+=("on"    "on"    "on"                         "on"    "off"        )
-		packages+=(gtk-theme-iris-dark-git lightdm-webkit-theme-bevel-git mpdnotify  )
-		defaults+=("on"                    "on"                           "on"       )
+		packages+=(dropbox numix-circle-icon-theme-git  gcalert google-chrome   )
+		defaults+=("on"    "on"                         "on"    "off"           )
+		packages+=(gtk-theme-iris-dark-git lightdm-webkit-theme-tendou          )
+		defaults+=("on"                    "on"                                 )
+		packages+=(mpd-notification-git                                         )
+		defaults+=("on"                                                         )
 	fi
 
 	checklist "Choose graphical software to install:" packages[@] defaults[@]
@@ -214,6 +182,13 @@ if $yes; then
 	for choice in $choices; do
 		choice=${packages[$choice]} # Get the name of the choice
 		mark_for_installation $choice
+
+		case $choice in
+		virtualbox)
+			# Install a related necessary package
+			mark_for_installation virtualbox-host-modules
+		;;
+		esac
 	done
 fi
 
@@ -232,35 +207,21 @@ for installed_package in "${to_install[@]}"; do
 	package=${to_install[$installed_package]} # Get the name of the package
 
 	case $installed_package in
-	dropbox)
-		# Set Dropbox to autostart
-		ensure_dir_exists ~/.config/autostart
-		declare -A dropbox_autostart
-		dropbox_autostart['config/autostart/dropbox.desktop']='.config/autostart/dropbox.desktop'
-		set_home_files_from_array dropbox_autostart
-	;;
 	feh)
 		# Add feh script
 		declare -A feh_script
 		feh_script['fehbg']='.fehbg'
-		set_home_files_from_array feh_script
-	;;
-	gcalert)
-		# Set gcalert to autostart
-		ensure_dir_exists ~/.config/autostart
-		declare -A gcalert_autostart
-		gcalert_autostart['config/autostart/Gcalert.desktop']='.config/autostart/Gcalert.desktop'
-		set_home_files_from_array gcalert_autostart
+		set_home_files_from_array feh_script $action_copy
 	;;
 	lightdm-gtk-greeter)
 		lightdm_service=true
 	;;
-	lightdm-webkit-theme-bevel-git)
-		# Set up the bevel theme
+	lightdm-webkit-theme-tendou)
+		# Set up the tendou theme
 		uncomment_line_in_file "greeter-session=example-gtk-gnome" /etc/lightdm/lightdm.conf '#'
-		replace_term_in_file 'example-gtk-gnome' 'lightdm-webkit-greeter' /etc/lightdm/lightdm.conf
-		replace_term_in_file '#user-session=default' 'user-session=xfce' /etc/lightdm/lightdm.conf
-		replace_term_in_file 'webkit-theme=webkit' 'webkit-theme=bevel' /etc/lightdm/lightdm-webkit*.conf
+		replace_term_in_file 'example-gtk-gnome' 'lightdm-webkit2-greeter' /etc/lightdm/lightdm.conf
+		replace_term_in_file '#user-session=default' 'user-session=bspwm' /etc/lightdm/lightdm.conf
+		replace_term_in_file 'webkit-theme=webkit' 'webkit-theme=tendou' /etc/lightdm/lightdm-webkit*.conf
 		lightdm_service=true
 	;;
 	offlineimap)
@@ -277,7 +238,15 @@ for installed_package in "${to_install[@]}"; do
 		program_box "systemctl --user enable offlineimap.service" "Running offlineimap in background"
 	;;
 	python-pip)
-		program_box "pip install --user pyglet" "Installing Pyglet"
+		program_box "pip install --user pyglet"   "Installing Pyglet"
+		program_box "pip install --user whizkers" "Installing Whizkers"
+
+		# Set up whizkers files
+		ensure_dir_exists ~/.config
+		declare -A whizkers_files
+		whizkers_files['config/whizkers']='.config/whizkers'
+		set_home_files_from_array whizkers_files
+		whizkers soft-pink
 	;;
 	python2-pip)
 		program_box "pip2 install --user pyglet" "Installing Pyglet"
@@ -303,10 +272,8 @@ fi
 # Linux Software Configs
 ########################################
 
-configs=( compton gcalert mpdnotify shell-scripts tint2 xfce4-notify)
-default=( "on"    "on"    "on"      "on"          "off" "on"        )
-configs+=(xfce4-terminal xfce4-weather-plugin xmonad xprofile       )
-default+=("on"           "on"                 "on"   "on"           )
+configs=( compton gcalert shell-scripts xorg-xinit        )
+default=( "on"    "on"    "on"          "on"              )
 
 checklist "Choose Linux config files to set up:" configs[@] default[@]
 
@@ -330,16 +297,6 @@ for choice in $choices; do
 		gcalert_files['config/gcalert/gcalertrc']='.config/gcalert/gcalertrc'
 		set_home_files_from_array gcalert_files
 		;;
-	mdpnotify)
-		infobox "Linking mpdnotify files"
-
-		ensure_dir_exists ~/.config/mpdnotify
-
-		declare -A mpdnotify_files
-		mpdnotify_files['config/mpdnotify/config']='.config/mpdnotify/config'
-		mpdnotify_files['config/mpdnotify/nocover.png']='.config/mpdnotify/nocover.png'
-		set_home_files_from_array mpdnotify_files
-		;;
 	shell-scripts)
 		infobox "Linking scripts into /usr/local/sbin/"
 		declare -A shell_scripts
@@ -347,63 +304,18 @@ for choice in $choices; do
 			shell_scripts["shell-scripts/$(basename $script)"]="/usr/local/sbin/$(basename $script)"
 		done
 
+		program_box "$($pkg_mgr --needed -S python2-numpy python2-scipy >/dev/tty)" "Installing necessary packages..."
+
 		sudo chown `whoami` /usr/local/sbin
 		set_files_from_array shell_scripts $action_copy
 		sudo chown root /usr/local/sbin
 		;;
-	tint2)
-		infobox "Linking tint2 config"
+	xinitrc)
+		infobox "Linking xinitrc config"
 
-		ensure_dir_exists ~/.config/tint2
-
-		declare -A tint2_files
-		tint2_files['config/tint2/default.tint2rc']='.config/tint2/tint2rc'
-		set_home_files_from_array tint2_files
-		;;
-	xfce4-notify)
-		infobox "Linking XFCE Terminal config"
-
-		ensure_dir_exists ~/.themes
-
-		declare -A xfce_notify_files
-		xfce_notify_files['themes/stiff']='.themes/stiff'
-		set_home_files_from_array xfce_notify_files
-		;;
-	xfce4-terminal)
-		# Download the font I use for my terminal config
-		program_box "$pkg_mgr --needed -S otf-meslo-powerline-git" "Installing terminal font..."
-
-		infobox "Linking XFCE Terminal config"
-
-		ensure_dir_exists ~/.config/xfce4/terminal
-
-		declare -A xfce_terminal_files
-		xfce_terminal_files['config/xfce4/terminal/accels.scm']='.config/xfce4/terminal/accels.scm'
-		xfce_terminal_files['config/xfce4/terminal/terminalrc']='.config/xfce4/terminal/terminalrc'
-		set_home_files_from_array xfce_terminal_files
-		;;
-	xfce4-weather-plugin)
-		# Set my weather icons
-		ensure_dir_exists ~/.config/xfce4/
-		declare -A weather_config
-		weather_config['config/xfce4/weather']='.config/xfce4/weather'
-		set_home_files_from_array weather_config
-	;;
-	xmonad)
-		infobox "Linking Xmonad config"
-
-		ensure_dir_exists ~/.xmonad
-
-		declare -A xmonad_files
-		xmonad_files['xmonad/xmonad.hs']='.xmonad/xmonad.hs'
-		set_home_files_from_array xmonad_files
-		;;
-	xprofile)
-		infobox "Linking xprofile config"
-
-		declare -A xprofile_files
-		xprofile_files['xprofile']='.xprofile'
-		set_home_files_from_array xprofile_files
+		declare -A xinitrc_files
+		xinitrc_files['xinitrc']='.xinitrc'
+		set_home_files_from_array xinitrc_files
 		;;
 	esac
 done
